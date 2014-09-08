3465ff122c85587d2065930717267158f28a2f7f
Moritz Walter <iamnotachoice@googlemail.com>
2014-09-08 18:16:18 +0200

    updated firmware

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -479,15 +479,15 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 // M501 - reads parameters from EEPROM (if you need reset them after you changed them temporarily).
 // M502 - reverts to the default "factory settings".  You still need to store them in EEPROM afterwards if you want to.
 // define this to enable eeprom support
-//#define EEPROM_SETTINGS
+define EEPROM_SETTINGS
 // to disable EEPROM Serial responses and decrease program space by ~1700 byte: comment this out:
 // please keep turned on if you can.
-//#define EEPROM_CHITCHAT
+define EEPROM_CHITCHAT
 
 // Preheat Constants
-#define PLA_PREHEAT_HOTEND_TEMP 180
-#define PLA_PREHEAT_HPB_TEMP 70
-#define PLA_PREHEAT_FAN_SPEED 255   // Insert Value between 0 and 255
+#define PLA_PREHEAT_HOTEND_TEMP 190
+#define PLA_PREHEAT_HPB_TEMP 80
+#define PLA_PREHEAT_FAN_SPEED 63   // Insert Value between 0 and 255
 
 #define ABS_PREHEAT_HOTEND_TEMP 255
 #define ABS_PREHEAT_HPB_TEMP 110
